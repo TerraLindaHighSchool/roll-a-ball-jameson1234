5e335829ebdb24040c4e418bf517d7c35614349c
jameson1234 <jg01626@srcs.org>
2021-01-07 14:49:30 -0800

    Tutorials Complete
    This is a lot to ask for but is it possible I can get full credit on this assignment I really need the points and I've had countless problems on this assignment holding me back. And thanks for the help today.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,35 +2,73 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 0;
 
-    private Rigidbody rb;
+	public float speed;
+	public TextMeshProUGUI countText;
+	public GameObject winTextObject;
 
-    private float movementX;
-    private float movementY;
+	private float movementX;
+	private float movementY;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        rb = GetComponent<Rigidbody>();
-    }
+	private Rigidbody rb;
+	private int count;
 
-    private void OnMove(InputValue movementValue)
-    {
-        Vector2 movementVector = movementValue.Get<Vector2>();
+	// At the start of the game..
+	void Start()
+	{
+		// Assign the Rigidbody component to our private rb variable
+		rb = GetComponent<Rigidbody>();
 
-        movementX = movementVector.x;
-        movementY = movementVector.y;
-    }
+		// Set the count to zero 
+		count = 0;
 
-    private void FixedUpdate()
-    {
-        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+		SetCountText();
 
-        rb.AddForce(movement * speed);
-    }
+		winTextObject.SetActive(false);
 
+	}
+
+	void FixedUpdate()
+	{
+		// Create a Vector3 variable, and assign X and Z to feature the horizontal and vertical float variables above
+		Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+
+		rb.AddForce(movement * speed);
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		// ..and if the GameObject you intersect has the tag 'Pick Up' assigned to it..
+		if (other.gameObject.CompareTag("PickUp"))
+		{
+			other.gameObject.SetActive(false);
+
+			// Add one to the score variable 'count'
+			count = count + 1; 
+
+			// Run the 'SetCountText()' function (see below)
+			SetCountText();
+		}
+	}
+
+	void OnMove(InputValue value)
+	{
+		Vector2 v = value.Get<Vector2>();
+
+		movementX = v.x;
+		movementY = v.y;
+	}
+
+	void SetCountText()
+	{
+        countText.text = "Count: " + count.ToString();
+		if(count >= 12)
+        {
+			winTextObject.SetActive(true);
+		}
+	}
 }
